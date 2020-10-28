f60f9c1d5fde25b0bb80b429d1b64209d48b22ac
ravi3222 <kashyap.ravi3222@gmail.com>
2020-10-29 04:14:39 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, SignIn, SignUp } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 function App() {
+  // const user = null; { name: "Ravi" };
+  const { user } = useAuthListener();
+  console.log(user);
+
   return (
     <>
       <Router>
-        <Route exact path={ROUTES.HOME}>
-          <Home />
-        </Route>
-        <Route exact path={ROUTES.BROWSE}>
-          <Browse />
-        </Route>
-        <Route exact path={ROUTES.SIGN_IN}>
-          <SignIn />
-        </Route>
-        <Route exact path={ROUTES.SIGN_UP}>
-          <SignUp />
-        </Route>
+        <Switch>
+          <IsUserRedirect
+            user={user}
+            loggedInPath={ROUTES.BROWSE}
+            path={ROUTES.SIGN_IN}
+          >
+            <SignIn />
+          </IsUserRedirect>
+          <IsUserRedirect
+            user={user}
+            loggedInPath={ROUTES.BROWSE}
+            path={ROUTES.SIGN_UP}
+          >
+            <SignUp />
+          </IsUserRedirect>
+          <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+            <Browse />
+          </ProtectedRoute>
+          <IsUserRedirect
+            user={user}
+            loggedInPath={ROUTES.BROWSE}
+            path={ROUTES.HOME}
+            exact
+          >
+            <Home />
+          </IsUserRedirect>
+        </Switch>
       </Router>
     </>
   );
